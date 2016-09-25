5f2584f0f5c7e924eeec695a3b9d90447a847eb4
Sylvia <sylviad1221@gmail.com>
2016-09-25 17:02:04 -0400

    add color change function

--- scripts/colorGame.js
+++ scripts/colorGame.js
@@ -8,24 +8,39 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
-var colorDisplay = document.getElementById("colorDisplay")
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var headingDisplay = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++){
   //add initial colors to squares
   squares[i].style.background = colors[i];
-
   //add click listeners to squares
   squares[i].addEventListener("click", function(){
     //grab color of picked square
     var clickedColor = this.style.background;
     //compare clicked color with picked color
     if (clickedColor === pickedColor) {
-      alert("You got it!");
+      messageDisplay.textContent = "Correct!";
+      changeColor(clickedColor);
     } else {
       this.style.background = "#232323";
+      messageDisplay.textContent = "Try again";
     }
   })
+}
+
+function changeColor(color){
+  headingDisplay.style.background = color;
+  for(var i = 0; i < squares.length; i++){
+    squares[i].style.background = color;
+  }
+}
+
+function pickColor(){
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
 }
