e3aa7385ed76c47e16efbd3e0220700b69e4142a
Sylvia <sylviad1221@gmail.com>
2016-09-25 19:58:09 -0400

    add reset button

--- scripts/colorGame.js
+++ scripts/colorGame.js
@@ -1,17 +1,10 @@
-var colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)"
-]
-
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
-var headingDisplay = document.querySelector("h1");
+var h1Display = document.querySelector("h1");
+var resetButton = document.querySelector("#reset")
 
 colorDisplay.textContent = pickedColor;
 
@@ -20,21 +13,23 @@ for(var i = 0; i < squares.length; i++){
   squares[i].style.background = colors[i];
   //add click listeners to squares
   squares[i].addEventListener("click", function(){
-    //grab color of picked square
-    var clickedColor = this.style.background;
-    //compare clicked color with picked color
-    if (clickedColor === pickedColor) {
-      messageDisplay.textContent = "Correct!";
-      changeColor(clickedColor);
-    } else {
-      this.style.background = "#232323";
-      messageDisplay.textContent = "Try again";
-    }
+      //grab color of picked square
+      var clickedColor = this.style.background;
+      //compare clicked color with picked color
+      console.log(clickedColor, pickedColor);
+      if (clickedColor === pickedColor) {
+        messageDisplay.textContent = "Correct!";
+        changeColor(clickedColor);
+        h1Display.style.background = clickedColor;
+        resetButton.textContent = "Play again?"
+      } else {
+        this.style.background = "#232323";
+        messageDisplay.textContent = "Try again";
+      }
   })
 }
 
 function changeColor(color){
-  headingDisplay.style.background = color;
   for(var i = 0; i < squares.length; i++){
     squares[i].style.background = color;
   }
@@ -44,3 +39,47 @@ function pickColor(){
   var random = Math.floor(Math.random() * colors.length);
   return colors[random];
 }
+
+function generateRandomColors(num){
+  //make an array
+  var arr = [];
+  //add num random colors to array num times
+  for(var i = 0; i < num; i++){
+    //get random color and push into array
+    arr.push(randomColor());
+  }
+  //return the array
+  return arr
+}
+
+function randomColor(){
+  //pick a "red" from 0-255
+  var r = Math.floor(Math.random() * 256);
+  //pick a "green" from 0-255
+  var g = Math.floor(Math.random() * 256);
+  //pick a "blue" from 0-255
+  var b = Math.floor(Math.random() * 256);
+  return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
+function squareColors() {
+  for(var i = 0; i < squares.length; i++){
+    //add initial colors to squares
+    squares[i].style.background = colors[i];
+  }
+}
+
+resetButton.addEventListener("click", function(){
+  //generate all new color
+  colors = generateRandomColors(6);
+  //pick a new random color from array
+  pickedColor = pickColor();
+  //change colorDisplay to match picked color
+  colorDisplay.textContent = pickedColor;
+  h1Display.style.background = "#232323";
+  //change colors of squares
+  squareColors();
+  //change button content
+  resetButton.textContent = "New Colors";
+  messageDisplay.textContent = " ";
+})
